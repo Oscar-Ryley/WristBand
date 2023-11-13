ea7a8d0486f489765b411f17d9a59afabb793aea
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-13 11:26:48 +0000

    Linked html to nodejs server
    Got linking to html file to work with nodejs server, css and js files also correctly linked within index.html
    
    JavaScript can now generate the set of cards with song data on them.

--- npm-init.js
+++ npm-init.js
@@ -2,36 +2,19 @@ const http = require("http");
 const express = require("express"); 
 const path = require("path");
 const app = express(); 
-app.use(express.static('client')); 
+const fs = require('fs')
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (req, res) => {
-    res.writeHead(200, "text/html");
-    const filePath = path.resolve(__dirname, 'index.html');
-    res.sendFile(filePath); 
+  fs.readFile('index.html', function(error, html){
+    if (error) {
+      res.writeHeader(403, {"Content-Type": "text/html"});
+      res.write('<h1>403 error file not found</h1>');
+    };
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
     res.end();
+``});
 }).listen(8080)
 
-app.get('/index', (req, res) => {
-    res.writeHead(200, "text/html");
-    const filePath = path.resolve(__dirname, 'index.html');
-    res.sendFile(filePath); 
-    res.end();
-})
-
 console.log('Server running at http://127.0.0.1:8080/'); 
-
-app.get('/random/:max', function(req, resp){ 
-  max = parseInt(req.params.max);
-  rand = Math.floor(Math.random()*max) +1;
-  console.log('Max via url is ' + max + ' rand is ' + rand);
-  resp.send('' + rand);
-});
-
-app.get('/r', function(req, resp){ 
-  max = parseInt(req.query.max); 
-  rand = Math.floor(Math.random()*max) +1;
-  console.log('Max via query is ' + max + ' rand is ' + rand); 
-  resp.send('' + rand);
-});
-
-app.listen(8090);
