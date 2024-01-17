3d2124b5d20a8a9cc64f432ce2a64590ec0d3859
Oscar Ryley <oscar.ryley@gmail.com>
2024-01-17 19:47:36 +0000

    HTML/CSS for Navbar and Profile bar done
    Need to implement requests to fetch the profile picture from the server depending on which user it is. Username, bio and post data already does this.
    
    Also need to work out how to output posts to a grid pattern.
    
    Two Modals are currently in use and can be accessed but do not achieve anything.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', async function (event) {
   try {
+    var numposts = 0;
     const response = await fetch('http://127.0.0.1:8080/songs/song');
     const body = (await response.json());
     for (i in body) {
@@ -7,12 +8,14 @@ window.addEventListener('load', async function (event) {
       this.document.getElementById('content').insertAdjacentHTML('afterend',
             `<div class="card song-container my-auto" style="width: 18rem;"><div class="card-body"><h4 class="card-title">${current[2]}</h4><h5 class="card-text">by ${current[3]}</h5><p class="card-text">Instrument: ${current[5]}<br> Lecture: ${current[1]}<br> Date: ${current[0]}<br> Musician: ${current[6]}</p> <a href="${current[4]}" target = "_blank" class="btn btn-outline-primary" id="link-button">Link to Song on Youtube</a></div></div><br>`
       );
+      numposts ++;
     }
     const user = await this.fetch('http://127.0.0.1:8080/user');
     const userdata = (await user.json());
     this.document.getElementById('username').innerHTML = userdata["username"];
     this.document.getElementById('profile-username').innerHTML = userdata["username"];
     this.document.getElementById('profile-bio').innerHTML = userdata["biography"];
+    this.document.getElementById('posts-number').innerHTML = numposts;
   } catch (e) {
     this.alert(e);
   }
