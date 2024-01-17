3672e150e4fc15a1683bbd06877f56d54744bf61
Oscar Ryley <oscar.ryley@gmail.com>
2024-01-17 20:06:39 +0000

    Song posts now display in grid format!
    Works by assigning to one of three columns iteratively.

--- public/client.js
+++ public/client.js
@@ -1,14 +1,19 @@
 window.addEventListener('load', async function (event) {
   try {
     var numposts = 0;
+    var column = 1;
     const response = await fetch('http://127.0.0.1:8080/songs/song');
     const body = (await response.json());
     for (i in body) {
       const current = body[i];
-      this.document.getElementById('content').insertAdjacentHTML('afterend',
-            `<div class="card song-container my-auto" style="width: 18rem;"><div class="card-body"><h4 class="card-title">${current[2]}</h4><h5 class="card-text">by ${current[3]}</h5><p class="card-text">Instrument: ${current[5]}<br> Lecture: ${current[1]}<br> Date: ${current[0]}<br> Musician: ${current[6]}</p> <a href="${current[4]}" target = "_blank" class="btn btn-outline-primary" id="link-button">Link to Song on Youtube</a></div></div><br>`
+      this.document.getElementById('content'+column).insertAdjacentHTML('afterBegin',
+            `<div class="card song-container my-auto" style="width: 18rem;"><div class="card-body"><h4 class="card-title">${current[2]}</h4><h5 class="card-text">by ${current[3]}</h5><p class="card-text"> Musician: ${current[6]} <br> Instrument: ${current[5]}<br> Date: ${current[0]}</p> <a href="${current[4]}" target = "_blank" class="btn btn-outline-primary" id="link-button">Link to Song on Youtube</a></div></div> <br><br>`
       );
       numposts ++;
+      column ++;
+      if (column == 4) {
+        column = 1;
+      };
     }
     const user = await this.fetch('http://127.0.0.1:8080/user');
     const userdata = (await user.json());
